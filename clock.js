973c792b2877c68d32a5eeaa0c29a10618e490ad
Greg <gregoryamitten@gmail.com>
2019-02-06 10:56:58 +0000

    Implemented offsets for each time unit

--- clock.js
+++ clock.js
@@ -1,36 +1,84 @@
 const ROTATION_INCREMENT = 360 / 60;
 
-const hourHand = document.getElementById("hour-hand");
-const minuteHand = document.getElementById("minute-hand");
-const secondHand = document.getElementById("second-hand");
+const hour = {
+  hand: document.getElementById("hour-hand"),
+  slider:  document.getElementById("hour-slider"),
+  text: document.getElementById("hour-offset-text")
+}
+
+const minute =  {
+  hand: document.getElementById("minute-hand"),
+  slider: document.getElementById("minute-slider"),
+  text: document.getElementById("minute-offset-text")
+}
+
+const second = {
+  hand: document.getElementById("second-hand"),
+  slider:  document.getElementById("second-slider"),
+  text: document.getElementById("second-offset-text")
+}
+
+let offset =  {
+  second: 0,
+  minute: 0,
+  hour: 0
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function convertToDigitalTime(date) {
-  return formatTime(date.getHours()) + ':'
-  + formatTime(date.getMinutes()) + ':'
-  + formatTime(date.getSeconds());
+  return formatTime(date.getHours(), offset.hour, 24) + ':'
+  + formatTime(date.getMinutes(), offset.minute, 60) + ':'
+  + formatTime(date.getSeconds(), offset.second, 60);
 }
 
-function formatTime(num) {
+function formatTime(num, offset, max) {
+  num = num + parseInt(offset)
+
   if(num < 10) return '0' + num
+  else if (num >= max) return formatTime(num - max, 0)
   return num;
 }
 
+second.slider.oninput = function() {
+  offset.second = this.value;
+  second.text.innerHTML = this.value;
+  updateClock();
+}
+
+minute.slider.oninput = function() {
+  offset.minute = this.value;
+  minute.text.innerHTML = this.value;
+  updateClock();
+}
+
+hour.slider.oninput = function() {
+  offset.hour = this.value;
+  hour.text.innerHTML = this.value;
+  updateClock();
+}
+
 const tick = async () => {
+  updateClock();
+  //To ensure seconds are accurate
+  await sleep(1000 - new Date().getMilliseconds());
+  tick();
+}
+
+function updateClock() {
   const time = new Date();
 
-  hourHand.setAttribute("transform", "rotate(" + (time.getHours() * (60 / 12)) * ROTATION_INCREMENT + " 200 200)");
-  minuteHand.setAttribute("transform", "rotate(" + time.getMinutes() * ROTATION_INCREMENT + " 200 200)");
-  secondHand.setAttribute("transform", "rotate(" + time.getSeconds() * ROTATION_INCREMENT + " 200 200)");
+  let secondRotation = (time.getSeconds() * ROTATION_INCREMENT) + (offset.second * ROTATION_INCREMENT)
+  let minuteRotation = (time.getMinutes() * ROTATION_INCREMENT) + (offset.minute * ROTATION_INCREMENT)
+  let hourRotation = (time.getHours() * (60 / 12) * ROTATION_INCREMENT) + (offset.minute * ROTATION_INCREMENT)
+
+  hour.hand.setAttribute("transform", "rotate(" + hourRotation + " 200 200)");
+  minute.hand.setAttribute("transform", "rotate(" + minuteRotation + " 200 200)");
+  second.hand.setAttribute("transform", "rotate(" + secondRotation + " 200 200)");
 
   document.getElementById("digital-clock").innerHTML = convertToDigitalTime(new Date());
-
-  //To ensure seconds are accurate
-  await sleep(1000 - time.getMilliseconds());
-  tick();
 }
 
 tick();
